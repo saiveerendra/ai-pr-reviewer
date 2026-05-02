7250070663600cb45cefb6c66b45f7b1114ffe9d
saiveerendra <113809676+saiveerendra@users.noreply.github.com>
2026-05-02 17:58:43 +0530

    Refactor test.py to simplify functionality
    Removed unnecessary functions and user input handling.

--- test.py
+++ test.py
@@ -7,21 +7,10 @@ def multiply(a,b):
 def substract(a,b):
     return abs(a-b)
     
-def run_command(user_input):
-    os.system(user_input)
 
-def slow_function(data):
-    for i in data:
-        for j in data:
-            print(i, j)  
 
 
 if __name__=='__main__':
-    user = input("Enter command: ")
-
-    run_command(user)       # triggers security agent
-    slow_function([1,2,3,4])  # triggers performance agent
-
     print(add(3, 2))
     print(substract(2, 3))
     print(multiply(2, 3))
